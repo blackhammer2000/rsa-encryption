a5cfc2cfbdd6cd8271edfe84f33b5a140501e4c6
wawerusamuel <waweruzamuel@gmail.com>
2022-07-06 20:20:37 +0300

    committed last changes to js-lessons

--- first.js
+++ first.js
@@ -1,4 +1,10 @@
-let num1 = 4;
-let num2 = 2;
+let num1 = "janey";
 
-console.log(num1 / num2);
+num1 = null;
+num1 += 5;
+console.log(num1);
+// if (num1 || num2) {
+//   console.table("both numbers are there");
+// } else {
+//   console.log("one of the numbers was not provided");
+// }

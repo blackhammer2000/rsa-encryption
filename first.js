5354a445cf5548a74d85d298b66fcfb5581ee537
wawerusamuel <waweruzamuel@gmail.com>
2022-07-06 13:29:15 +0300

    first commit, created first.js

---
+++ first.js
@@ -0,0 +1,20 @@
+// let name = "ROBERT";
+// console.log(name);
+// console.log(name);
+// console.log(name);
+// console.log(name);
+// console.log(name);
+// console.log(name);
+// console.log(name);
+// console.log(name);
+
+console.log("ROBERT");
+console.log("ROBERT");
+console.log("ROBERT");
+console.log("ROBERT");
+console.log("ROBERT");
+console.log("ROBERT");
+console.log("ROBERT");
+console.log("ROBERT");
+console.log("ROBERT");
+console.log("ROBERT");

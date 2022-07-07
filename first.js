ff283885df70318cf1f6cc7bc712a9d2f06c213d
wawerusamuel <waweruzamuel@gmail.com>
2022-07-07 20:50:29 +0300

    Finished the decrypt function and everything is working well...

--- first.js
+++ first.js
@@ -1,9 +1,16 @@
 const encrypt = (data, standard1, standard2) => {
-  const asciiCode = data.charCodeAt();
-  const encryptedDataAscii = Math.pow(asciiCode, standard1) % standard2;
-  return encryptedDataAscii;
+  const encryptedData = Math.pow(data, standard1) % standard2;
+  return encryptedData;
 };
 
-const decryptedMessage = (data, standard1, standard2) => {};
+const decryptedMessage = (data, standard1, standard2) => {
+  const decryptedData = Math.ceil(Math.pow(data, standard1) % standard2);
+  return decryptedData;
+};
+
+const eMessage = encrypt(2, 5, 14);
+const dMessage = encrypt(eMessage, 20, 14);
+
+console.log(eMessage, dMessage);
 
 // const formula =  19d (%ff) = 1

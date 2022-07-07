70f5b61968fbcde9a48f6eaa06d8101981c63b0c
wawerusamuel <waweruzamuel@gmail.com>
2022-07-07 20:34:26 +0300

    started working onthe RSA encryption and the encryption function is done

--- first.js
+++ first.js
@@ -1,22 +1,9 @@
-let num1 = "janey";
+const encrypt = (data, standard1, standard2) => {
+  const asciiCode = data.charCodeAt();
+  const encryptedDataAscii = Math.pow(asciiCode, standard1) % standard2;
+  return encryptedDataAscii;
+};
 
-// if (num1 || num2) {
-//   console.table("both numbers are there");
-// } else {
-//   console.log("one of the numbers was not provided");
-// }
-const multiples19 = [];
-const multiples24 = [];
-
-for (let i = 19; i < 100; i++) {
-  multiples19.push(i * 19);
-}
-for (let i = 1; i < 100; i++) {
-  multiples24.push(i * 24);
-}
+const decryptedMessage = (data, standard1, standard2) => {};
 
 // const formula =  19d (%ff) = 1
-
-let result = 0;
-
-console.log(multiples19, multiples24);

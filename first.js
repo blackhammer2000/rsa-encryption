887aadd76f07c0d23cc50ac82337e98a187c3101
wawerusamuel <waweruzamuel@gmail.com>
2022-07-07 20:09:05 +0300

    did some changes to first.js...

--- first.js
+++ first.js
@@ -1,10 +1,22 @@
 let num1 = "janey";
 
-num1 = null;
-num1 += 5;
-console.log(num1);
 // if (num1 || num2) {
 //   console.table("both numbers are there");
 // } else {
 //   console.log("one of the numbers was not provided");
 // }
+const multiples19 = [];
+const multiples24 = [];
+
+for (let i = 19; i < 100; i++) {
+  multiples19.push(i * 19);
+}
+for (let i = 1; i < 100; i++) {
+  multiples24.push(i * 24);
+}
+
+// const formula =  19d (%ff) = 1
+
+let result = 0;
+
+console.log(multiples19, multiples24);

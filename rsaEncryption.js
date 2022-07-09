8ae83d2e554e638386341db055ddf4358bc0729d
wawerusamuel <waweruzamuel@gmail.com>
2022-07-09 11:54:51 +0300

    successfully implemented both the encryptionKey generator and the decryptionKey generator and the first test case has passed....

--- rsaEncryption.js
+++ rsaEncryption.js
@@ -4,16 +4,16 @@ function generateEncryptionKey(standard1, standard2) {
 
   let coPrimeNumbers = [];
 
-  for (let number = 1; number <= product_N; number++) {
+  for (let number = 1; number <= fineFactor; number++) {
     if (
       number % standard1 !== 0 &&
       number % standard2 !== 0 &&
-      product_N % number !== 0
+      fineFactor % number !== 0
     )
       coPrimeNumbers.push(number);
   }
 
-  console.log(coPrimeNumbers);
+  // console.log(coPrimeNumbers);
 
   const encryptionNumber = coPrimeNumbers.find((number) => {
     if (product_N % number !== 0 && fineFactor % number !== 0) return number;
@@ -21,36 +21,45 @@ function generateEncryptionKey(standard1, standard2) {
 
   return [encryptionNumber, product_N, fineFactor];
 }
-
-let encKey = generateEncryptionKey(2, 7);
-console.log(encKey);
+let encryptionKey = generateEncryptionKey(2, 7);
+console.log(encryptionKey);
 
 /////////////RSA DECRYPTION KEY GENERATOR////////////////////////
 
-const generateDecryptionKey = (encryptionKey, fineFactor) => {
-  // formula = (finefactor * dkey + 1) / ekey -- should not be a float.
+const generateDecryptionKey = (encryptionKey) => {
+  let decryptionKey = 1;
 
-  let m = 1;
-
-  // while(m){
-  //   const
-  // }
-
-  // return decryptionKey;
+  while (decryptionKey) {
+    if (
+      (decryptionKey * encryptionKey[0]) % encryptionKey[2] === 1 &&
+      decryptionKey !== encryptionKey[0]
+    )
+      return [decryptionKey, encryptionKey[1]];
+    decryptionKey++;
+  }
 };
 
-console.log(generateDecryptionKey(encKey[0], encKey[2]));
+const decryptionKey = generateDecryptionKey(encryptionKey);
+console.log(decryptionKey);
 
 //////////////////////////////////
 
-// const encrypt = (data, encryptionKey) => {
+// const encryptMessage = (data, encryptionKey) => {
 //   const encryptedData = Math.pow(data, encryptionKey[0]) % encryptionKey[1];
 //   return encryptedData;
 // };
 
+// const message = encryptMessage(2, encryptionKey);
+// console.log(message);
+
 //////////////////////////////////
 
-// const decryptedMessage = (data, encryptionKey) => {
-//   const decryptedData = Math.ceil(Math.pow(data, encryptionKey[0]) % encryptionKey[1]);
+// const decryptMessage = (data, decryptionKey) => {
+//   const decryptedData = Math.ceil(
+//     Math.pow(data, decryptionKey[0]) % decryptionKey[1]
+//   );
 //   return decryptedData;
 // };
+
+// const dMessage = decryptMessage(message, decryptionKey);
+// console.log(dMessage);
